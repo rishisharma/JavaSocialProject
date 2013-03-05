c0b037add7504723cefca5cf205aff0ad6db8f2f
rishisharma <rishisharma@192.168.1.7>
2013-03-05 21:54:12 +0530

    Creating new project

---
+++ JavaSocialProject/src/test/java/com/rishi/social/AppTest.java
@@ -0,0 +1,19 @@
+package com.rishi.social;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
